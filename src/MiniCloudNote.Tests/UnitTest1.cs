baaa887803ece455d31fc17092ae0d55c17cf1cb
Phạm Hồng Thái <0127067@st.huce.edu.vn>
2025-12-17 00:33:02 +0700

    Day 39: Implement TaxCalculator and breaking 0% coverage

--- src/MiniCloudNote.Tests/UnitTest1.cs
+++ src/MiniCloudNote.Tests/UnitTest1.cs
@@ -1,22 +1,22 @@
 ﻿using System.Reflection;
 using Xunit;
+using MiniCloudNote.Core;
 
 namespace MiniCloudNote.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test_1_Cong_1_Bang_2()
+    public void Tinh_Thue_Luong_Cao_Phai_Dung()
     {
-        // 1. Arrange (Chuẩn bị)
-        int a = 1;
-        int b = 1;
+        // 1. Arrange
+        var calc = new TaxCalculator();
+        decimal luong = 20_000_000; // 20 triệu
 
-        // 2. Act (Hành động)
-        int result = a + b;
+        // 2. Act (Hành động gọi sang Core)
+        decimal thue = calc.CalculateTax(luong);
 
-        // 3. Assert (Khẳng định kết quả)
-        // Nếu result không phải là 2 -> Test Failed -> Jenkins Báo lỗi
-        Assert.Equal(2, result);
+        // 3. Assert (20 triệu * 10% = 2 triệu)
+        Assert.Equal(2_000_000, thue);
     }
 }
