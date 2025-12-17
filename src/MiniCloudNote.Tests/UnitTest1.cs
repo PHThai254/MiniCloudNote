b882ef885f73857b122e947ce0435691e990ae55
Phạm Hồng Thái <0127067@st.huce.edu.vn>
2025-12-17 10:54:23 +0700

    Update UnitTest1.cs

--- src/MiniCloudNote.Tests/UnitTest1.cs
+++ src/MiniCloudNote.Tests/UnitTest1.cs
@@ -9,14 +9,28 @@ public class UnitTest1
     [Fact]
     public void Tinh_Thue_Luong_Cao_Phai_Dung()
     {
-        // 1. Arrange
+        // Arrange
         var calc = new TaxCalculator();
         decimal luong = 20_000_000; // 20 triệu
 
-        // 2. Act (Hành động gọi sang Core)
+        // Act (Hành động gọi sang Core)
         decimal thue = calc.CalculateTax(luong);
 
-        // 3. Assert (20 triệu * 10% = 2 triệu)
+        // Assert (20 triệu * 10% = 2 triệu)
         Assert.Equal(2_000_000, thue);
+    }
+
+    [Fact]
+    public void Tinh_Thue_Luong_Thap_Duoc_Mien_Thue()
+    {
+        // Arrange
+        var calc = new TaxCalculator();
+        decimal luong = 5_000_000; // 5 triệu (Nhỏ hơn 10tr -> Sẽ đi vào nhánh False)
+
+        // Act 
+        decimal thue = calc.CalculateTax(luong);
+
+        // Assert
+        Assert.Equal(0, thue); // Kỳ vọng dòng 'return 0' sẽ chạy
     }
 }
